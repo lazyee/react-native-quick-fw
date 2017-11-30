0edb1cd9d18ea93f1987998566a2f1f51f952d73
leekajun <378229364@qq.com>
2017-11-30 16:21:07 +0800

    Revert "移除无用的引用"
    This reverts commit b261cc61a17843470fcad8b485ef4e33173210bb.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
+import {
+    View,
+} from 'react-native';
 import {configureStore} from './app/Store';
 import {Provider} from 'react-redux';
+import TestUser from './components/test/TestUserForRedux';
 import AppWithNavigationState from './components/routers/AppWithNavigationState';
 
 const {persistor, store} = configureStore();
